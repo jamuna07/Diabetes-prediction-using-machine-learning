c05ee5718e7f354f78e76225ce393bdc14adc596
jamuna07 <66872772+jamuna07@users.noreply.github.com>
2020-07-27 19:39:17 +0530

    Add files via upload

---
+++ diabetes.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+diabetes = pd.read_csv(r'C:\Users\GANESAN\Downloads\diabetes.csv')
+print(diabetes.columns)
+diabetes.head()
+
+
+# In[3]:
+
+
+print("dimension of diabetes data: {}".format(diabetes.shape))
+
+
+# In[4]:
+
+
+print(diabetes.groupby('Outcome').size())
+
+
+# In[4]:
+
+
+import seaborn as sns
+sns.countplot(diabetes['Outcome'],label="Count")
+
+
+# In[5]:
+
+
+diabetes.info()
+
+
+# In[6]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+from sklearn.neighbors import KNeighborsClassifier
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    # build the model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(knn.score(X_train, y_train))
+    # record test set accuracy
+    test_accuracy.append(knn.score(X_test, y_test))
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+plt.savefig('knn_compare_model')
+
+
+# In[7]:
+
+
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test, y_test)))
+
+
+# In[8]:
+
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression().fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
+
+
+# In[9]:
+
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train, y_train)
+print("Training set accuracy: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("Test set accuracy: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+
+# In[10]:
+
+
+logreg100 = LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set accuracy: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("Test set accuracy: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+
+# In[11]:
+
+
+diabetes_features = [x for i,x in enumerate(diabetes.columns) if i!=8]
+plt.figure(figsize=(8,6))
+plt.plot(logreg.coef_.T, 'o', label="C=1")
+plt.plot(logreg100.coef_.T, '^', label="C=100")
+plt.plot(logreg001.coef_.T, 'v', label="C=0.001")
+plt.xticks(range(diabetes.shape[1]), diabetes_features, rotation=90)
+plt.hlines(0, 0, diabetes.shape[1])
+plt.ylim(-5, 5)
+plt.xlabel("Feature")
+plt.ylabel("Coefficient magnitude")
+plt.legend()
+plt.savefig('log_coef')
+
+
+# In[12]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+
+# In[13]:
+
+
+tree = DecisionTreeClassifier(max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+
+# In[14]:
+
+
+print("Feature importances:\n{}".format(tree.feature_importances_))
+
+
+# In[15]:
+
+
+def plot_feature_importances_diabetes(model):
+    plt.figure(figsize=(8,6))
+    n_features = 8
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), diabetes_features)
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature")
+    plt.ylim(-1, n_features)
+plot_feature_importances_diabetes(tree)
+plt.savefig('feature_importance')
+
+
+# In[16]:
+
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=100, random_state=0)
+rf.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf.score(X_test, y_test)))
+
+
+# In[17]:
+
+
+rf1 = RandomForestClassifier(max_depth=3, n_estimators=100, random_state=0)
+rf1.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf1.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf1.score(X_test, y_test)))
+
+
+# In[18]:
+
+
+plot_feature_importances_diabetes(rf)
+
+
+# In[19]:
+
+
+from sklearn.ensemble import GradientBoostingClassifier
+gb = GradientBoostingClassifier(random_state=0)
+gb.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb.score(X_test, y_test)))
+
+
+# In[20]:
+
+
+gb1 = GradientBoostingClassifier(random_state=0, max_depth=1)
+gb1.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb1.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb1.score(X_test, y_test)))
+
+
+# In[21]:
+
+
+gb2 = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gb2.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb2.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb2.score(X_test, y_test)))
+
+
+# In[22]:
+
+
+plot_feature_importances_diabetes(gb1)
+
+
+# In[23]:
+
+
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(X_train, y_train)
+print("Accuracy on training set: {:.2f}".format(svc.score(X_train, y_train)))
+print("Accuracy on test set: {:.2f}".format(svc.score(X_test, y_test)))
+
+
+# In[24]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+svc = SVC()
+svc.fit(X_train_scaled, y_train)
+print("Accuracy on training set: {:.2f}".format(svc.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.2f}".format(svc.score(X_test_scaled, y_test)))
+
+
+# In[25]:
+
+
+svc = SVC(C=1000)
+svc.fit(X_train_scaled, y_train)
+print("Accuracy on training set: {:.3f}".format(
+    svc.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.3f}".format(svc.score(X_test_scaled, y_test)))
+
+
+# In[26]:
+
+
+from sklearn.neural_network import MLPClassifier
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+print("Accuracy on training set: {:.2f}".format(mlp.score(X_train, y_train)))
+print("Accuracy on test set: {:.2f}".format(mlp.score(X_test, y_test)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
